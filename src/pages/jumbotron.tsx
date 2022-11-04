451022cf47d8668a7a470bd62fed37970201cc99
AsZaychik <aszaychik@gmail.com>
2022-11-04 23:48:36 +0700

    JUMBOTRON TEST

--- src/pages/jumbotron.tsx
+++ src/pages/jumbotron.tsx
@@ -1,6 +1,6 @@
 export default function Jumbotron() {
   return (
-    <main className="text-center">
+    <main className="text-center w-full h-36 bg-neutral-100 flex justify-center items-center">
       Jumbotron
     </main>
   );
